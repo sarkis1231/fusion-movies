1430ea41da189934f064300e77148b0c6532d988
sarkis1231 <sakooghly@gmail.com>
2021-07-20 18:56:58 +0400

    Added functionality for movie details

--- components/features/movies/movie-file.jsx
+++ components/features/movies/movie-file.jsx
@@ -3,15 +3,31 @@ import PropTypes from "fusion:prop-types"
 
 
 class MovieDetail extends Component {
+    state = {
+        isPlotShown: false
+    }
+    togglePlot = () =>  {
+        this.setState(({isPlotShown}) => ({isPlotShown: !isPlotShown}))
+    }
+
 
     render() {
-        console.log(this.props)
+        const {isPlotShown} = this.state
+        const plotButton = (
+            <button onClick={this.togglePlot}>
+                {isPlotShown ? "Hide Plot" : "Show Plot"}
+            </button>
+        );
+
+        const plot = "Lorem Ipsum";
+
         return(
             <div className="movie-detail col-sm-12 col-md-18">
                 <h1>Jurassic Park</h1>
                 <p><strong>Director:</strong> Steven Spielberg</p>
                 <p><strong>Actors:</strong>Sam Nail, Laura Dern</p>
                 <p><strong>Plot:</strong>Lorem Ipsum</p>
+                <p><strong>Plot:</strong>{plotButton} {isPlotShown && plot} </p>
             </div>
         )
     }
