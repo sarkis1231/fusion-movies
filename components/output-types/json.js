bc36e4e81827e402db739e123fdf156a0b6736cb
sarkis1231 <sakooghly@gmail.com>
2021-07-23 15:24:36 +0400

    Refactoring

--- components/output-types/json.js
+++ components/output-types/json.js
@@ -1,6 +1,7 @@
 const Json = ({children}) => {
-    // return Array.isArray(children) ? children[0] : null;
+
     return JSON.stringify(children, null,  4)
+    
 }
 
 Json.contentType = 'application/json';
