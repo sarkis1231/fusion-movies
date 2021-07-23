807d229c0576e0039c49f832eb7e749da4668594
sarkis1231 <sakooghly@gmail.com>
2021-07-23 14:20:26 +0400

    Refactoring

--- components/output-types/json.js
+++ components/output-types/json.js
@@ -1,5 +1,4 @@
 const Json = ({children}) => {
-    console.log("output Type JSON: " + children)
     // return Array.isArray(children) ? children[0] : null;
     return JSON.stringify(children, null,  4)
 }
